9ef3c9e1059e7e227251c52096cfcd523c1831b5
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-07-19 17:34:02 +0900

    Commit

--- src/floatingPoint.java
+++ src/floatingPoint.java
@@ -1,32 +1,19 @@
-import java.math.BigDecimal;
-
 public class floatingPoint {
     public static void main(String[] args) {
         floatingPoint ftPoint = new floatingPoint();
         ftPoint.additive();
     }
     public void additive(){
-//        float var1 = 1.325f;
-//        float var2 = 1.253f;
-//        double var3 = 1.264;
-//        double var4 = 1.354;
+        float var1 = 1.325f;
+        float var2 = 1.253f;
+        double var3 = 1.264;
+        double var4 = 1.354;
 
-//        float sum1 = var1 + var1;
-//        System.out.println(sum1); //2.65
+        float sum1 = var1 + var1;
+        System.out.println("float => " + sum1);  //float => 2.65
 
-//        double sum2 = var3 + var4;
-//        System.out.println(sum2); //2.6180000000000003
-
-        long start = System.currentTimeMillis();
-
-        for(int i = 0; i <= 100000; i++){
-            float var1 = 1.325f;
-            float var2 = 1.253f;
-            float sum1 = var1 + var1;
-            System.out.println("덧셈(+) :" +sum1); //2.65
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("SDB에서 노드생성까지의 실행시간 : " + (end - start)/1000.0);
+        double sum2 = var3 + var4;
+        System.out.println("double => " + sum2); //double => 2.6180000000000003
 
     }
 }
