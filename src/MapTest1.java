4ed7ec19a155b015d0c1a5a7c32172d2f5b011e4
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-08-10 22:52:13 +0900

    Commit

---
+++ src/MapTest1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MapTest1 {
+    public static void main(String[] args) {
+        MapTest1 mapT1 = new MapTest1();
+        mapT1.testMap1();
+    }
+    public void testMap1(){
+        Map<Integer,String> map = new HashMap<>();
+
+        map.put(1,"Test1");
+        map.put(2,"Test2");
+        map.put(1,"Test3");
+        System.out.println(map); // {1=Test3, 2=Test2}
+    }
+}
