a4866af34e93e355ae693784de44c1d5839a2b7c
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-08-10 22:30:23 +0900

    Commit

--- src/MapTest.java
+++ src/MapTest.java
@@ -1,22 +1,34 @@
 import java.util.HashMap;
 
 public class MapTest {
-    String test;
+    String phone;
+    String name;
     public MapTest(){
+    }
+    public MapTest(String phone, String name){
+        this.phone = phone;
+        this.name = name;
+    }
 
+    public String getPhone(){
+        return phone;
     }
-    public MapTest(String test){
-        this.test = test;
+
+    public String getName(){
+        return name;
     }
+
     public static void main(String[] args) {
         MapTest mapT = new MapTest();
         mapT.testMap1();
     }
     public void testMap1(){
-       MapTest mapT1 = new MapTest("test");
-        HashMap<MapTest,String> map = new HashMap<>();
+       MapTest mapT1 = new MapTest();
+       mapT1.phone = "010-1234-5678";
+       mapT1.name = "홍길동";
+       HashMap<String, String> map = new HashMap<>();
 
-        map.put(mapT1,"A");
-        System.out.println(map);
+       map.put(mapT1.phone,mapT1.name);
+       System.out.println(map);
     }
 }
