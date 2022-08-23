dc199e6dcdc39b440ae367c74714c05348b2396a
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-08-23 23:52:25 +0900

    Commit

---
+++ src/c/enums/EnumTest.java
@@ -0,0 +1,12 @@
+package c.enums;
+
+public class EnumTest {
+    public enum FourSeasons {
+        Spring, Summer, Autumn, Winter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FourSeasons.Autumn);
+    }
+}
+
