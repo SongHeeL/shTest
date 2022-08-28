3faba44f144042983be82c58e9063bf0b4ef3f88
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-08-28 18:48:01 +0900

    Commit

--- src/c/stream/ManageTextFile.java
+++ src/c/stream/ManageTextFile.java
@@ -7,45 +7,75 @@ public class ManageTextFile {
     public static void main(String[] args) {
         ManageTextFile manager = new ManageTextFile();
         int numberCount = 10;
-        String fullPath = "C:"+separator+"godofjava"+separator+"text"+separator+"numbers.txt";
-        String fullPath1 = "C:"+separator+"godofjava"+separator+"text"+separator+"numbers1.txt";
-        manager.writeFile(fullPath,numberCount);
+        String fullPath = "C:" + separator + "godofjava" + separator + "text" + separator + "numbers.txt";
+        String fullPath1 = "C:" + separator + "godofjava" + separator + "text" + separator + "numbers1.txt";
+        manager.writeFile(fullPath, numberCount);
         manager.readFile(fullPath1);
     }
-    public void writeFile(String fileName, int numberCount){
+
+    public void writeFile(String fileName, int numberCount) {
         FileWriter fileWriter = null;
         BufferedWriter bufferedWriter = null;
         try {
             fileWriter = new FileWriter(fileName);
             bufferedWriter = new BufferedWriter(fileWriter);
-            for(int loop = 0; loop <= numberCount; loop++){
+            for (int loop = 0; loop <= numberCount; loop++) {
                 bufferedWriter.write(Integer.toString(loop));
                 bufferedWriter.newLine();
             }
             System.out.println("Write success !!!");
         } catch (IOException ion) {
             ion.printStackTrace();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
         }
     }
 
-    public void readFile(String fileName){
+    public void readFile(String fileName) {
         FileReader fileReader = null;
         BufferedReader bufferedReader = null;
         try {
             fileReader = new FileReader(fileName);
             bufferedReader = new BufferedReader(fileReader);
             String data;
-            while ((data = bufferedReader.readLine()) != null){
+            while ((data = bufferedReader.readLine()) != null) {
                 System.out.println(data);
             }
             System.out.println("Read success !!!");
         } catch (IOException ion) {
             ion.printStackTrace();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
         }
     }
-
 }
