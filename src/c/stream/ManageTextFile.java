93c339950bb06bee64ae3e141aa50cbca8cc7846
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-08-28 18:34:04 +0900

    Commit

---
+++ src/c/stream/ManageTextFile.java
@@ -0,0 +1,51 @@
+package c.stream;
+
+import static java.io.File.separator;
+import java.io.*;
+
+public class ManageTextFile {
+    public static void main(String[] args) {
+        ManageTextFile manager = new ManageTextFile();
+        int numberCount = 10;
+        String fullPath = "C:"+separator+"godofjava"+separator+"text"+separator+"numbers.txt";
+        String fullPath1 = "C:"+separator+"godofjava"+separator+"text"+separator+"numbers1.txt";
+        manager.writeFile(fullPath,numberCount);
+        manager.readFile(fullPath1);
+    }
+    public void writeFile(String fileName, int numberCount){
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            fileWriter = new FileWriter(fileName);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            for(int loop = 0; loop <= numberCount; loop++){
+                bufferedWriter.write(Integer.toString(loop));
+                bufferedWriter.newLine();
+            }
+            System.out.println("Write success !!!");
+        } catch (IOException ion) {
+            ion.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(String fileName){
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(fileName);
+            bufferedReader = new BufferedReader(fileReader);
+            String data;
+            while ((data = bufferedReader.readLine()) != null){
+                System.out.println(data);
+            }
+            System.out.println("Read success !!!");
+        } catch (IOException ion) {
+            ion.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
