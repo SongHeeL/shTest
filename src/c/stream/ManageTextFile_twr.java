201a91e1d5fd63016c8da31f642f9e4f04797f8a
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2022-09-03 00:10:28 +0900

    Commit

--- src/c/stream/ManageTextFile_twr.java
+++ src/c/stream/ManageTextFile_twr.java
@@ -9,9 +9,9 @@ public class ManageTextFile_twr {
     public static void main(String[] args) {
         ManageTextFile_twr manager = new ManageTextFile_twr();
         String fullPath = "C:" + separator + "godofjava" + separator + "text" + separator + "numbers.txt";
-        manager.writeFile(fullPath);
+        manager.readFile(fullPath);
     }
-    public void writeFile(String fileName) {
+    public void readFile(String fileName) {
         try(Scanner sc = new Scanner(new File(fileName))) {
            System.out.println(sc.nextLine());
 
